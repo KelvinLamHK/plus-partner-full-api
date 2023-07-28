f8440215c928dbeafe6ae1c658a58ccd1c8c1c38
KelvinLamHK <kelvinlamtatyin@gmail.com>
2023-07-28 10:54:51 +0800

    Update LogConfig.java

--- src/main/java/com/ftlife/plus/partner/configuration/LogConfig.java
+++ src/main/java/com/ftlife/plus/partner/configuration/LogConfig.java
@@ -16,4 +16,13 @@ import org.springframework.stereotype.Component;
 public class LogConfig {
     private String logUrl;
     private String applicationName;
+//    Internal:
+//    https://flexuat.interconnect.systems/plus-partner-int/
+//    client_id: a2517c3b7e06485fa89375eec2530e53
+//    client_secret: 93f72319688D4EFdB43Ea15Bfe56B62A
+//
+//    External:
+//    https://flexuat.interconnect.systems/plus-partner-ext/
+//    client_id: 56ac48f8228b4602a56097d390b91786
+//    client_secret: 5fFe2DE79D4247288f8BE2462ebd7Bb4
 }
